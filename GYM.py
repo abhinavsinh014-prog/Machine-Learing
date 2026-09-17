24327fe82619448df62ccf61ee3c3987a1e781ed
Abhinav singh <abhinavsinh014@gmail.com>
2026-09-17 22:58:26 +0530

    adding a new file

--- GYM.py
+++ GYM.py
@@ -9,21 +9,85 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 import streamlit as st
 
+st.set_page_config(
+    page_title="AI Muscle Growth Predictor",
+    page_icon="💪",
+    layout="wide"
+)
+
+
 RANDOM_SEED = 42
-rng = np.random.default_rng(RANDOM_SEED)
+
+MUSCLES = [
+    "Biceps",
+    "Triceps",
+    "Shoulders",
+    "Chest",
+    "Back",
+    "Quads",
+    "Hamstrings",
+    "Calves"
+]
+
 
 MUSCLE_PARAMS = {
-    "Biceps":     {"Gmax": 6.0,  "k": 0.045, "mrv_time": 70,  "recovery_penalty": 0.020},
-    "Triceps":    {"Gmax": 6.5,  "k": 0.040, "mrv_time": 75,  "recovery_penalty": 0.018},
-    "Shoulders":  {"Gmax": 7.0,  "k": 0.030, "mrv_time": 90,  "recovery_penalty": 0.015},
-    "Chest":      {"Gmax": 8.0,  "k": 0.026, "mrv_time": 100, "recovery_penalty": 0.014},
-    "Back":       {"Gmax": 9.0,  "k": 0.020, "mrv_time": 130, "recovery_penalty": 0.010},
-    "Quads":      {"Gmax": 10.0, "k": 0.017, "mrv_time": 150, "recovery_penalty": 0.009},
-    "Hamstrings": {"Gmax": 8.5,  "k": 0.022, "mrv_time": 110, "recovery_penalty": 0.012},
-    "Calves":     {"Gmax": 5.0,  "k": 0.035, "mrv_time": 80,  "recovery_penalty": 0.022},
+    "Biceps": {
+        "Gmax": 6.0,
+        "k": 0.045,
+        "mrv_time": 70,
+        "recovery_penalty": 0.020
+    },
+
+    "Triceps": {
+        "Gmax": 6.5,
+        "k": 0.040,
+        "mrv_time": 75,
+        "recovery_penalty": 0.018
+    },
+
+    "Shoulders": {
+        "Gmax": 7.0,
+        "k": 0.030,
+        "mrv_time": 90,
+        "recovery_penalty": 0.015
+    },
+
+    "Chest": {
+        "Gmax": 8.0,
+        "k": 0.026,
+        "mrv_time": 100,
+        "recovery_penalty": 0.014
+    },
+
+    "Back": {
+        "Gmax": 9.0,
+        "k": 0.020,
+        "mrv_time": 130,
+        "recovery_penalty": 0.010
+    },
+
+    "Quads": {
+        "Gmax": 10.0,
+        "k": 0.017,
+        "mrv_time": 150,
+        "recovery_penalty": 0.009
+    },
+
+    "Hamstrings": {
+        "Gmax": 8.5,
+        "k": 0.022,
+        "mrv_time": 110,
+        "recovery_penalty": 0.012
+    },
+
+    "Calves": {
+        "Gmax": 5.0,
+        "k": 0.035,
+        "mrv_time": 80,
+        "recovery_penalty": 0.022
+    }
 }
- 
-MUSCLES = list(MUSCLE_PARAMS.keys())
+
 
 def true_growth_function(muscle, weekly_time, sessions_per_week, intensity_pct, training_age):
     
@@ -148,4 +212,4 @@ my_input = pd.DataFrame({
 })
 
 predicted_growth = model.predict(my_input)
-st.write(f"Predicted growth: {predicted_growth[0]:.2f}%")
+st.write(f"Predicted growth: {predicted_growth[0]:.2f}%")-
